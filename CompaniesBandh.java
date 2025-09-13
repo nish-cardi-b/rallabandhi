f1559b9b219796ed5972170a73cd971064367997
nish-cardi-b <cardonishb@gmail.com>
2025-09-13 19:11:32 +0800

    a

--- CompaniesBandh.java
+++ CompaniesBandh.java
@@ -1,2 +1,7 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class CompaniesBandh {
+    public static void main(String[] args) {
+        System.out.println("ghar pe chhape maare - we are on sajid khan branch");
+    }
 }
