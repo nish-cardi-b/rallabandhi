631835a8d72c1a976f99154af9e7031923810049
nish-cardi-b <cardonishb@gmail.com>
2025-09-13 19:15:05 +0800

    2-commit

--- CompaniesBandh.java
+++ CompaniesBandh.java
@@ -4,5 +4,6 @@ public class CompaniesBandh {
     public static void main(String[] args) {
         System.out.println("now we are on flop-film");
         System.out.println("1-commit");
+        System.out.println("2-commit");
     }
 }
